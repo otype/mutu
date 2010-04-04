734b2b74c83ec737a425e1e7c78b80066dc75bde
Hans-Gunther Schmidt <hans@otype.de>
2010-04-04 03:20:10 +0200

    committing for transfer

---
+++ SSHLib.h
@@ -0,0 +1,16 @@
+//
+//  SSHLib.h
+//  Mutu
+//
+//  Created by Hans-Gunther Schmidt on 13.03.10.
+//  Copyright 2010 Otype. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface SSHLib : NSObject {
+
+}
+
+@end
---
+++ SSHLib.m
@@ -0,0 +1,14 @@
+//
+//  SSHLib.m
+//  Mutu
+//
+//  Created by Hans-Gunther Schmidt on 13.03.10.
+//  Copyright 2010 Otype. All rights reserved.
+//
+
+#import "SSHLib.h"
+
+
+@implementation SSHLib
+
+@end
